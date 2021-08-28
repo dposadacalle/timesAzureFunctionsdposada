e58ad633594274aeb2748e056ce318d6e4849f6d
Daniel Posada Calle <posadacalled@gmail.com>
2021-08-28 18:38:03 -0500

    Add the module to class library

---
+++ timesAzureFunctionsdposada/timesAzureFunctionsdposada.Common/Models/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace timesAzureFunctionsdposada.Common.Models
+{
+    public class Time
+    {
+        public int IdEmpleo { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Tipo { get; set; }
+        public bool Consolidado { get; set; }
+    }
+}
